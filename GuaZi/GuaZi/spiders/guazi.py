995aec30ebc2a6eaa1dbd3e658100d1e6d9ad0b6
wangjiegui <1220593517@qq.com>
2021-10-12 12:21:27 +0800

    瓜子框架补充

--- GuaZi/GuaZi/spiders/guazi.py
+++ GuaZi/GuaZi/spiders/guazi.py
@@ -73,9 +73,11 @@ class GuaziSpider(scrapy.Spider):
         #                              'family_name': family_name,
         #                              'tag': tag
         #                          })
+
         for car in json_data['data']['postList']:
             clue_id = car['clue_id']
             try:
+                # 取严选车
                 guarantee = car['title_tags']['text']
             except:
                 guarantee = None
@@ -96,17 +98,69 @@ class GuaziSpider(scrapy.Spider):
         guarantee = response.meta['guarantee']
         html = etree.HTML(str(response.body, encoding="utf-8"))
         print(html.xpath('//h4[@class="car-name"]/text()'))
-        '''
-        第一页要取数据的解析 
-        
-        '''
+
+        # 第一页要取数据的解析
+        info_dict = {}
+        divs = html.xpath('//div[@class="item"]')
+        for div in divs:
+            key_name = div.xpath('./p[1]/text()')[0].strip()
+            value_name = div.xpath('./p[2]/text()')[0].strip()
+            #     print(key_name, value_name)
+            info_dict[key_name] = value_name
+        # 注册时间
+        registeryear=html.xpath('//div[@class="m-car-record__body--top"]/div[@class="item"][1]/p[1]/text()')[0]\
+            .strip()
+        # 里程
+        mileage=html.xpath('//div[@class="m-car-record__body--top"]/div[@class="item"][2]/p[@class="text"]/text()')[0]\
+            .strip()
+        # 排放标准
+        emission=html.xpath('//div[@class="m-car-record__body--top"]/div[@class="item"][3]/p[@class="text"]/text()')[0]\
+            .strip()
+        # 过户次数
+        change_times=info_dict['过户次数']
+        # 排量
+        output=info_dict['排量']
+        # 城市归属地
+        city=info_dict['车牌归属地']
+        # 变速箱类型
+        geartype=info_dict['变速箱']
+        # 年检到期时间
+        yearchecktime=info_dict['年检到期']
+        # 车辆用途
+        usage=info_dict['使用性质']
+        # 交强险
+        insurance1_date=info_dict['交强险到期']
+        # 颜色
+        color=info_dict['车身颜色']
+        # 出厂日期
+        produceyear=info_dict['出厂日期']
+        # 车源号
+        statusplus=info_dict['车源号']
+        # 钥匙
+        keynumbers=info_dict['钥匙数量']
+
         url = 'https://m.guazi.com/car-record/v2?clueId={}'.format(clue_id)
         yield scrapy.Request(url=url, callback=self.parse_parameter, dont_filter=True,
                              meta={
                                  'brand_name': brand_name,
                                  'family_name': family_name,
                                  'clue_id': clue_id,
-                                 'guarantee': guarantee
+                                 'guarantee': guarantee,
+                                 'registeryear':registeryear,
+                                 'mileage':mileage,
+                                 'emission':emission,
+                                 'change_times':change_times,
+                                 'output':output,
+                                 'city':city,
+                                 'geartype':geartype,
+                                 'yearchecktime':yearchecktime,
+                                 'usage':usage,
+                                 'insurance1_date':insurance1_date,
+                                 'color':color,
+                                 'produceyear':produceyear,
+                                 'statusplus':statusplus,
+                                 'keynumbers':keynumbers,
+
                              })
 
     def parse_parameter(self, response):
@@ -115,6 +169,20 @@ class GuaziSpider(scrapy.Spider):
         family_name = response.meta['family_name']
         clue_id = response.meta['clue_id']
         guarantee = response.meta['guarantee']
+        registeryear=response.meta['registeryear']
+        mileage=response.meta['mileage']
+        emission=response.meta['emission']
+        change_times=response.meta['change_times']
+        output=response.meta['output']
+        city=response.meta['city']
+        geartype=response.meta['geartype']
+        yearchecktime=response.meta['yearchecktime']
+        usage=response.meta['usage']
+        insurance1_date=response.meta['insurance1_date']
+        color=response.meta['color']
+        produceyear=response.meta['produceyear']
+        statusplus=response.meta['statusplus']
+        keynumbers=response.meta['keynumbers']
         html = etree.HTML(str(response.body, encoding="utf-8"))
         parameter_dict = {}
         configures = html.xpath('//li[@class="list-item__body__row"]')
@@ -124,15 +192,65 @@ class GuaziSpider(scrapy.Spider):
                 value_name1 = configure.xpath('./div[@class="list-item__body__row__content"]/text()')[0].strip()
             except:
                 try:
-                    value_name1 = configure.xpath('./div[@class="list-item__body__row__content"]/span/@class')[
-                        0].replace('list-item__body__row__content--', '')
+                    value_name1 = configure.xpath('./div[@class="list-item__body__row__content"]/span/@class')[0]\
+                        .replace('list-item__body__row__content--', '')
                 except:
                     value_name1 = ''.join(
-                        configure.xpath('./div[@class="list-item__body__row__content"]//text()')).replace('\n',
-                                                                                                          '').replace(
+                        configure.xpath('./div[@class="list-item__body__row__content"]//text()'))\
+                        .replace('\n','').replace(
                         ' ', '')
             parameter_dict[key_name1] = value_name1
+        # familyname生产厂家
+        factoryname = parameter_dict['厂商']
+        # 指导价(含税)
+        guidepricetax = parameter_dict['厂商指导价(万元)']
+        # 年款
+        makeyear=parameter_dict['上市时间']
+        # 燃油类型
+        fueltype = parameter_dict['能源形式']
+        # 变速箱描述
+        gear = parameter_dict['变速器描述']
+        # 车身类型
+        bodystyle = parameter_dict['车身形式']
+        # 进气方式
+        first_owner=parameter_dict['进气形式']
+        # 气缸排列型式
+        lwv = parameter_dict['气缸排列形式']
+        # 气缸数
+        lwvnumber = parameter_dict['气缸数(个)']
+        # 最大马力
+        maxps = parameter_dict['最大马力(Ps)']
+        # 最大功率
+        maxpower = parameter_dict['最大功率(kW)']
+        # 最大扭矩
+        maxnm = parameter_dict['最大扭矩(N·m)']
+        # 燃油标号
+        fuelnumber = parameter_dict['燃油标号']
+        # 档位数
+        gearnumber = parameter_dict['挡位个数']
+        # 轴距
+        wheelbase = parameter_dict['轴距(mm)']
+        # 车高
+        height = parameter_dict['高度(mm)']
+        # 车宽
+        width=parameter_dict['宽度(mm)']
+        # 车长
+        length = parameter_dict['长度(mm)']
+        # 前轮距
+        frontgauge = parameter_dict['前轮距(mm)']
+        # 车门数量
+        doors = parameter_dict['车门数(个)']
+        # 座位数
+        seats = parameter_dict['座位数(个)']
+        # 整备质量
+        weight = parameter_dict['整备质量(kg)']
+        # 驱动类型
+        driverway = parameter_dict['驱动方式']
+        # 描述
+        desc=
 
         item['carid'] = clue_id
         item['car_source'] = 'guazi'
         update_time = None
+
+
