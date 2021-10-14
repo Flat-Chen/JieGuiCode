bcb612fb094e908fd87cee7609e018c8555e3156
wangjiegui <1220593517@qq.com>
2021-10-14 18:13:38 +0800

    爱卡汽车论坛2

--- xcar/xcar/middlewares.py
+++ xcar/xcar/middlewares.py
@@ -3,101 +3,232 @@
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
+# from scrapy import signals
+#
+# # useful for handling different item types with a single interface
+# from itemadapter import is_item, ItemAdapter
+#
+#
+# class XcarSpiderMiddleware:
+#     # Not all methods need to be defined. If a method is not defined,
+#     # scrapy acts as if the spider middleware does not modify the
+#     # passed objects.
+#
+#     @classmethod
+#     def from_crawler(cls, crawler):
+#         # This method is used by Scrapy to create your spiders.
+#         s = cls()
+#         crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
+#         return s
+#
+#     def process_spider_input(self, response, spider):
+#         # Called for each response that goes through the spider
+#         # middleware and into the spider.
+#
+#         # Should return None or raise an exception.
+#         return None
+#
+#     def process_spider_output(self, response, result, spider):
+#         # Called with the results returned from the Spider, after
+#         # it has processed the response.
+#
+#         # Must return an iterable of Request, or item objects.
+#         for i in result:
+#             yield i
+#
+#     def process_spider_exception(self, response, exception, spider):
+#         # Called when a spider or process_spider_input() method
+#         # (from other spider middleware) raises an exception.
+#
+#         # Should return either None or an iterable of Request or item objects.
+#         pass
+#
+#     def process_start_requests(self, start_requests, spider):
+#         # Called with the start requests of the spider, and works
+#         # similarly to the process_spider_output() method, except
+#         # that it doesn’t have a response associated.
+#
+#         # Must return only requests (not items).
+#         for r in start_requests:
+#             yield r
+#
+#     def spider_opened(self, spider):
+#         spider.logger.info('Spider opened: %s' % spider.name)
+#
+#
+# class XcarDownloaderMiddleware:
+#     # Not all methods need to be defined. If a method is not defined,
+#     # scrapy acts as if the downloader middleware does not modify the
+#     # passed objects.
+#
+#     @classmethod
+#     def from_crawler(cls, crawler):
+#         # This method is used by Scrapy to create your spiders.
+#         s = cls()
+#         crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
+#         return s
+#
+#     def process_request(self, request, spider):
+#         # Called for each request that goes through the downloader
+#         # middleware.
+#
+#         # Must either:
+#         # - return None: continue processing this request
+#         # - or return a Response object
+#         # - or return a Request object
+#         # - or raise IgnoreRequest: process_exception() methods of
+#         #   installed downloader middleware will be called
+#         return None
+#
+#     def process_response(self, request, response, spider):
+#         # Called with the response returned from the downloader.
+#
+#         # Must either;
+#         # - return a Response object
+#         # - return a Request object
+#         # - or raise IgnoreRequest
+#         return response
+#
+#     def process_exception(self, request, exception, spider):
+#         # Called when a download handler or a process_request()
+#         # (from other downloader middleware) raises an exception.
+#
+#         # Must either:
+#         # - return None: continue processing this exception
+#         # - return a Response object: stops process_exception() chain
+#         # - return a Request object: stops process_exception() chain
+#         pass
+#
+#     def spider_opened(self, spider):
+#         spider.logger.info('Spider opened: %s' % spider.name)
+
+
+import logging
+import time
+
+import requests
 from scrapy import signals
-
+import random
 # useful for handling different item types with a single interface
-from itemadapter import is_item, ItemAdapter
+# from itemadapter import is_item, ItemAdapter
+
+from scrapy.downloadermiddlewares.useragent import UserAgentMiddleware
+from scrapy import signals
+from selenium import webdriver
+from scrapy.http import HtmlResponse, TextResponse
+from selenium.webdriver import Chrome, ActionChains, FirefoxProfile
+from selenium.webdriver.chrome.options import Options
+from twisted.internet.error import TimeoutError, TCPTimedOutError
 
 
-class XcarSpiderMiddleware:
-    # Not all methods need to be defined. If a method is not defined,
-    # scrapy acts as if the spider middleware does not modify the
-    # passed objects.
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        # This method is used by Scrapy to create your spiders.
-        s = cls()
-        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
-        return s
-
-    def process_spider_input(self, response, spider):
-        # Called for each response that goes through the spider
-        # middleware and into the spider.
-
-        # Should return None or raise an exception.
-        return None
-
-    def process_spider_output(self, response, result, spider):
-        # Called with the results returned from the Spider, after
-        # it has processed the response.
-
-        # Must return an iterable of Request, or item objects.
-        for i in result:
-            yield i
-
-    def process_spider_exception(self, response, exception, spider):
-        # Called when a spider or process_spider_input() method
-        # (from other spider middleware) raises an exception.
-
-        # Should return either None or an iterable of Request or item objects.
-        pass
-
-    def process_start_requests(self, start_requests, spider):
-        # Called with the start requests of the spider, and works
-        # similarly to the process_spider_output() method, except
-        # that it doesn’t have a response associated.
-
-        # Must return only requests (not items).
-        for r in start_requests:
-            yield r
-
-    def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
-
-
-class XcarDownloaderMiddleware:
-    # Not all methods need to be defined. If a method is not defined,
-    # scrapy acts as if the downloader middleware does not modify the
-    # passed objects.
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        # This method is used by Scrapy to create your spiders.
-        s = cls()
-        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
-        return s
-
-    def process_request(self, request, spider):
-        # Called for each request that goes through the downloader
-        # middleware.
-
-        # Must either:
-        # - return None: continue processing this request
-        # - or return a Response object
-        # - or return a Request object
-        # - or raise IgnoreRequest: process_exception() methods of
-        #   installed downloader middleware will be called
-        return None
-
-    def process_response(self, request, response, spider):
-        # Called with the response returned from the downloader.
-
-        # Must either;
-        # - return a Response object
-        # - return a Request object
-        # - or raise IgnoreRequest
-        return response
+class ProxyMiddleware(object):
+    def __init__(self):
+        self.count = 0
+        self.proxy = "http://" + getProxy()
 
     def process_exception(self, request, exception, spider):
-        # Called when a download handler or a process_request()
-        # (from other downloader middleware) raises an exception.
+        if isinstance(exception, TimeoutError):
+            self.proxy = "http://" + getProxy()
+            request.meta['proxy'] = self.proxy
+            return request
 
-        # Must either:
-        # - return None: continue processing this exception
-        # - return a Response object: stops process_exception() chain
-        # - return a Request object: stops process_exception() chain
-        pass
+    def process_request(self, request, spider):
+        proxy = getProxy()
+        request.meta['proxy'] = "http://" + proxy
 
-    def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+
+def getProxy():
+    s = requests.session()
+    s.keep_alive = False
+    url_list = ['http://192.168.2.120:5000']
+    # random.shuffle(url_list)
+    url = url_list[0]
+    headers = {
+        'Connection': 'close',
+    }
+    proxy = s.get(url, headers=headers, auth=('admin', 'zd123456')).text[0:-6]
+    return proxy
+
+
+class UserAgent(UserAgentMiddleware):
+    def process_request(self, request, spider):
+        ua = random.choice(user_agent_list)
+        request.headers.setdefault('User-Agent', ua)
+
+
+user_agent_list = [
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+    "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
+    "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+]
+
+
+
+
+# class GuaZiSeleniumMiddleware(object):
+#     """
+#         selenium 动态加载代理ip 、实现点击
+#     """
+#
+#
+    #     # 擦除浏览器指纹
+    #     try:
+    #         with open('stealth.min.js') as f:
+    #             self.clean_js = f.read()
+    #     except:
+    #         with open('/home/stealth.min.js') as f:
+    #             self.clean_js = f.read()
+    #
+    #     self.browser.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+    #         "source": self.clean_js
+    #     })
+    #
+    # def close_spider(self, spider):
+    #     try:
+    #         self.browser.close()
+    #     except:
+    #         pass
+    #
+    # def __del__(self):
+    #     try:
+    #         self.browser.quit()
+    #         self.browser.close()
+    #     except:
+    #         pass
+    #
+    # def process_request(self, request, spider):
+    #     if 'm.guazi.com' in request.url:
+    #         # logging.info('！！！！！！用浏览器打开获取渲染后的网页源代码！！！！！')
+    #         # 此处访问要请求的url
+    #         try:
+    #             for i in range(3):
+    #                 self.browser.get(request.url)
+    #                 for i in range(15):
+    #                     time.sleep(0.2)
+    #                     if '正在努力加载' not in self.browser.page_source:
+    #                         break
+    #                 if '正在努力加载' not in self.browser.page_source:
+    #                     break
+    #             url = self.browser.current_url
+    #             body = self.browser.page_source
+    #         except:
+    #             logging.error("加载页面太慢，停止加载，继续下一步操作")
+    #             self.browser.execute_script("window.stop()")
+    #             url = request.url
+    #             body = self.browser.page_source
+    #         return TextResponse(url=url, body=body, encoding="utf-8", request=request)
--- xcar/xcar/spiders/xcar1.py
+++ xcar/xcar/spiders/xcar1.py
@@ -1,11 +1,31 @@
+# import logging
+import html
+import random
+import json
+from lxml import etree
+import re
 import scrapy
 
 
 class Xcar1Spider(scrapy.Spider):
     name = 'xcar1'
     allowed_domains = ['xcar.com.cn']
-    start_urls = "https://www.xcar.com.cn/bbs/#R"
+    start_urls = ["https://www.xcar.com.cn/bbs/"]
+    brand_list = ['名爵', '荣威', '上海', '大众', '上汽大通MAXUS', '别克', '凯迪拉克', '雪佛兰', '宝骏', '五菱', '斯柯达']
 
+    factory = {'名爵': '上汽乘用车',
+               '荣威': '上汽乘用车',
+               '上海': '上汽乘用车',
+               '斯柯达': '上汽大众',
+               '大众': '上汽大众',
+               '上汽大通MAXUS': '上汽大通',
+               '别克': '上汽通用',
+               '凯迪拉克': '上汽通用',
+               '雪佛兰': '上汽通用',
+               '宝骏': '上汽通用五菱',
+               '五菱': '上汽通用五菱',
+               }
+    band_series = "斯柯达速尊论坛昕锐论明锐旅行版论坛探岳论坛新宝来论坛大众SMV论坛大众ID.4 CROZZ论坛大众二手件交流区大众ID.6 CROZZ论坛大众CC论坛高尔夫论坛高尔夫嘉旅论坛GTI论坛捷达论坛新捷达论坛开迪论坛|兄迪连揽境论坛迈腾论坛新速腾论坛探影论坛探歌论坛探岳GTE论坛蔚领论坛Amarok论坛大众论坛-进口大众Atlas论坛大众R-Model论坛大众蔚揽论坛大众XL1论坛大众EOS论坛大众凯路威论坛大众迈特威论坛大众ARTEON论坛Golf旅行轿车论坛大众新能源论坛高尔夫R论坛辉腾论坛甲壳虫论坛迈腾旅行轿车论坛R36论坛尚酷论坛尚酷R论坛途锐论坛大众up!论坛夏朗论坛昂科雷论坛别克荣御论坛凯迪拉克论坛凯迪拉克SRX论坛凯迪拉克CTS论坛凯迪拉克凯雷德科迈罗论坛斯帕可论坛Traverse论坛雪佛兰SS论坛雪佛兰论坛-进口雪佛兰沃蓝达Volt论"
     @classmethod
     def update_settings(cls, settings):
         settings.setdict(
@@ -14,16 +34,16 @@ class Xcar1Spider(scrapy.Spider):
 
     is_debug = True
     custom_debug_settings = {
-        'MYSQL_SERVER': '192.168.1.94',
-        'MYSQL_USER': "dataUser94",
-        'MYSQL_PWD': "94dataUser@2020",
-        'MYSQL_PORT': 3306,
-        'MYSQL_DB': 'usedcar_update',
-        'MYSQL_TABLE': '',
+        # 'MYSQL_SERVER': '192.168.1.94',
+        # 'MYSQL_USER': "dataUser94",
+        # 'MYSQL_PWD': "94dataUser@2020",
+        # 'MYSQL_PORT': 3306,
+        # 'MYSQL_DB': 'saicnqms',
+        # 'MYSQL_TABLE': 'luntan_all_copyjg1',
         'MONGODB_SERVER': '127.0.0.1',
         'MONGODB_PORT': 27017,
         'MONGODB_DB': 'xcar',
-        'MONGODB_COLLECTION': '',
+        'MONGODB_COLLECTION': 'xcar_luntan',
         'CONCURRENT_REQUESTS': 8,
         'DOWNLOAD_DELAY': 0,
         'LOG_LEVEL': 'DEBUG',
@@ -33,16 +53,138 @@ class Xcar1Spider(scrapy.Spider):
     }
 
 
-    brand_list = ['名爵', '荣威', '上海', '大众', '上汽大通MAXUS', '别克', '凯迪拉克', '雪佛兰', '宝骏', '五菱', '斯柯达']
-
     def __init__(self, **kwargs):
         super(Xcar1Spider, self).__init__()
         self.headers = {
             'user-agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36"}
         self.counts = 0
+        self.carnum = 2000000
 
-
-
-
+    # 实现翻页，并且进入下一个循环
     def parse(self, response):
-        pass
+        luntans_url_list = response.xpath("//table")
+        random.shuffle(luntans_url_list)
+        for luntan_url in luntans_url_list:
+            # 品牌的名字
+            brand = luntan_url.xpath(".//tbody/tr/td/a/text()").extract_first()
+            if brand not in self.brand_list:
+                continue
+            luntan_url_list = luntan_url.xpath(
+                ".//div[@class='t0922_pidiva']/div[@class='t1203_fbox']//a/@href").extract()
+            luntan_name_list = luntan_url.xpath(
+                ".//div[@class='t0922_pidiva']/div[@class='t1203_fbox']//a/text()").extract()
+            for i in range(len(luntan_name_list)):
+                if luntan_name_list[i] in self.band_series:
+                    continue
+                print(brand, luntan_name_list[i])
+                fid_id = re.findall(r"//www.xcar.com.cn/bbs/forumdisplay.php\?fid=(\d*)", luntan_url_list[i])[0]
+                url = "https://www.xcar.com.cn/bbs/xbbsapi/forumdisplay/get_thread_list.php?fid={}&orderby=lastpost&page=1".format(
+                    fid_id)
+                yield response.follow(url=url, headers=self.headers,
+                                      callback=self.get_luntan,
+                                      meta={"user_car": luntan_name_list[i],
+                                            "page": 1,
+                                            "fid_id": fid_id,
+                                            "brand": brand,
+                                            "factory": self.factory[brand]
+                                            },)
+
+
+
+
+    #
+    def get_luntan(self, response, ):
+        # print(response.meta["brand"], response.meta["user_car"], "*" * 50)
+        brand=response.meta["brand"]
+        luntan_name_list[i]=response.meta["user-car"]
+        response.meta["page"] = response.meta["page"] + 1
+        json_data = json.loads(response.text)
+        for luntan_list in json_data['data']['data']['thread_list']:
+            luntan_id_list=luntan_list['tid']
+            url = "http://www.xcar.com.cn/bbs/xbbsapi/forumdisplay/get_thread_list.php?fid={}&orderby=lastpost&page={}".format(
+                response.meta["fid_id"], response.meta["page"])
+            yield scrapy.Request(url=url, callback=self.get_luntan, headers=self.headers, meta=response.meta)
+            for luntan_id in luntan_id_list:
+                url = "http://www.xcar.com.cn/bbs/viewthread.php?tid={}".format(luntan_id)
+                # valid = self.redis_tools(url)
+                # if valid == 0:
+                #     logging.log(msg="this http request is repetition", level=logging.INFO)
+                #     continue
+                # else:
+                yield scrapy.Request(url=url, callback=self.parse_luntan, headers=self.headers,
+                                         meta=response.meta)
+    #
+    # def parse_luntan(self, response):
+    #
+    #     content=html.xpath('//div[@class="floor_div floor_div_e"]/div[@id="content"]/div/text()')[0]
+    #     user_name=html.xpath('')
+
+    # def get_luntan(self, response):
+    #     # user_car = response.meta["luntan_name_list[i]"]
+    #     # factory = response.meta["self.factory[brand]"]
+    #     print(response.meta["brand"], response.meta["user_car"], "*" * 50)
+    #     response.meta["page"] = response.meta["page"] + 1
+    #     # print(response.meta)
+    #     luntan_id_list = response.text.encode('utf8').decode('unicode_escape').replace(
+    #         r'"videos":"{"num":0,"list":[]}",', "")
+    #     luntan_id_list = re.findall(r'"tid":(\d*),"subject"', luntan_id_list)
+    #     if luntan_id_list == []:
+    #         return
+    #     else:
+    #         url = "http://www.xcar.com.cn/bbs/xbbsapi/forumdisplay/get_thread_list.php?fid={}&orderby=lastpost&page={}".format(
+    #             response.meta["fid_id"], response.meta["page"])
+    #         yield scrapy.Request(url=url, callback=self.get_luntan, headers=self.headers, meta=response.meta)
+    #         for luntan_id in luntan_id_list:
+    #             url = "http://www.xcar.com.cn/bbs/viewthread.php?tid={}".format(luntan_id)
+    #             valid = self.redis_tools(url)
+    #             if valid == 0:
+    #                 logging.log(msg="this http request is repetition", level=logging.INFO)
+    #                 continue
+    #             else:
+    #                 yield scrapy.Request(url=url, callback=self.parse_luntan, headers=self.headers,
+    #                                      meta=response.meta)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def parse(self, response):
+#     json_data = json.loads(response.text)
+#     for brand_id in json_data['var _cartype'].keys():
+#         for forum_list in json_data['var _cartype'][forum_list]:
+#             forum_name=forum_list['forumName']
+#             forum_id=forum_list['forumId']
+#             url='view-source:https://www.xcar.com.cn/bbs/forumdisplay.php?fid={}'.format(forum_id)
+#             yield scrapy.Request(url=url, callback=self.,
+#                                  meta={
+#                                      'forum_name': forum_name,
+#                                  })
+
+
+
+
+
